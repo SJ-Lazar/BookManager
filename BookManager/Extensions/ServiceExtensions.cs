6c906711722703c8448edd3779ad7564cf22b832
SJ-Lazar <78497706+SJ-Lazar@users.noreply.github.com>
2021-06-13 01:31:55 +0200

    Configured Swagger and Swagger UI
    Added Swagger.AspNetCore Nuget Package.
    Created Static ServiceExtensions Class.
    Created ConfigureSwagger Method in Service Extension.
    Invoked ConfigureSwagger in startup class configure service.
    Changed Launch Settings to Swagger.

---
+++ BookManager/Extensions/ServiceExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookManager.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Book Manager API", Version = "v1" });
+            });
+        }
+    }
+}
